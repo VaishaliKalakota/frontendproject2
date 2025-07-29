4434d8f6b2b991e3574ce94e9d58d9c4bf67946b
VaishaliKalakota <2400030153@kluniversity.in>
2025-07-29 15:17:56 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import{Link}from'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome To FrontEnd class in KLU</h1>
+      <h2>Today's topic is introduction to React</h2>
+      <h3>Today's date is 29.07.2025</h3>
+      <h4>VRRA</h4>
+      <h5>2400030153 - vaishali</h5>
+      <Link to="/page1">Go To Page1</Link>
+      <Link to="/page2">Go To Page2</Link>
+      <Link to="/page3">Go To Page3</Link>
+    </>
+  )
+}
+
+export default App
---
+++ src/page1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>India is my country</h1>
+        <Link to="/">Go To Homepage</Link>
+      </div>
+    )
+  }
+}
